748afd2547f17e571bb2b06b481c7d82987ed7a0
Megaf Cassini <mmegaf@gmail.com>
2022-04-03 12:55:20 +0100

    Improves code documentation. Fixes #3

--- BoxTextLib.sh
+++ BoxTextLib.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # BoxTextLib.sh
 
-# Author; Megaf
+# Author; Megaf <mmegaf@gmail.com>, <https://github.com/Megaf>,
 # Creation date: 03 - April - 2022
 # License: GNU General Public License v3.0
 
@@ -10,23 +10,44 @@
 # that will stand out from other text.
 # You can use it to attract the users attention.
 
-# Use:
-# There are a number of styles available, the default is just a white/invisible box.
-# To pick a style you TODO.
+# Usage:
+# To display a text message in the console using BoxTextLib.sh you simply replace
+# "echo" or "printf" from your shell script with "./BoxTextLib.sh" or "./box"(TODO).
+#
+# For example, to show the message "test", you write in your code "./BoxTextLib.sh test".
 
+# Styles:
+# There are a number of styles available, the default is just a white/invisible box border..
+# To pick a style you, you can either pass the style name as command argument or
+# define the variable "style" to the string for the name of the style..
+#
+# For example:
+# message="This is a Message." ./BoxTextLib.sh boxsolid
+# The output will be:
+# █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+# █  This is a Message.  █
+# █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+#
+# Or, you can do: message="This is a message" style=boxsolid ./BoxTextLib.sh
+# which will have the same effect.
+
+# Say function.
+# This function is used to "draw" the border around the text given by the $message variable.
 say () {
   length=$(expr length "$message")
 
-  spacesize="2"
+  spacesize="2" # Change this number to increase or decrease empty space before and after the message.
   linesize=$(expr $spacesize + $length + $spacesize)
 
-  tput sgr0
+  tput sgr0 # Clears terminal's colours and styles to default.
 
+  # The lines below will print elements of the box and the variable $message.
   printf "$topleftcorner"; printf "%0.s$top" $(seq 1 $linesize); printf "$toprightcorner\n"
   printf "$leftside"; printf "%0.s " $(seq 1 $spacesize); tput bold; printf "$message"; tput sgr0; printf "%0.s " $(seq 1 $spacesize); printf "$rightside\n"
   printf "$bottomleftcorner"; printf "%0.s$bottom" $(seq 1 $linesize); printf "$bottomrightcorner\n"
 }
 
+# Ugly nested "if" conditions to define which style should be used.
 if [ "$*" = "boxdoubleline" ] || [ "$style" = "boxdoubleline" ]
   then
     topleftcorner="╔"
@@ -86,4 +107,5 @@ if [ "$*" = "boxdoubleline" ] || [ "$style" = "boxdoubleline" ]
       fi
     fi
 fi
+# Calls "say" function to finally print the message with the box.
 say
